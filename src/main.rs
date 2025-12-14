14ae85048f5ca6a0276e0c7ec64cff3a58597752
johnslavik <bartosz@slawecki.dev>
2025-12-14 22:21:23 +0100

    Print help page if stdin is a TTY

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,27 @@
-use std::io::{Read, Write};
+use clap::{CommandFactory, Parser};
+use std::io::{IsTerminal, Read, Write};
+use std::str::from_utf8;
 
 use base64::engine::general_purpose as base64_engines;
 use base64::Engine;
 
+#[derive(Parser)]
+/// Copy to clipboard using ANSI OSC52 sequence
+struct Args {
+    /// File to read from; if not provided, stdin is read
+    filename: Option<String>,
+    #[arg(short, long, default_value_t = false)]
+    /// Don't echo the copied content
+    silent: bool,
+}
+
 fn main() -> anyhow::Result<()> {
-    let args = {
-        use clap::Parser;
-        #[derive(Parser)]
-        /// Copy to clipboard using ANSI OSC52 sequence
-        struct Args {
-            /// File to read from; if not provided, stdin is read
-            filename: Option<String>,
-        }
-        Args::parse()
-    };
+    if std::io::stdin().is_terminal() {
+        Args::command().print_help().unwrap_or(());
+        return Ok(());
+    }
+
+    let args = Args::parse();
 
     let contents = {
         match args.filename {
@@ -39,5 +47,8 @@ fn main() -> anyhow::Result<()> {
         stdout.flush()?;
     }
 
+    if !args.silent {
+        println!("Copied: {:?}", from_utf8(&contents)?)
+    }
     Ok(())
 }
